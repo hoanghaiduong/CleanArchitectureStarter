9ab6c934d72ddc23c256fba6ad28aa5309634d80
hoanghaiduong <hoanghaiduong1711@gmail.com>
2024-08-11 23:52:24 +0700

    Starter

--- src/Application/Common/Behaviours/PerformanceBehaviour.cs
+++ src/Application/Common/Behaviours/PerformanceBehaviour.cs
@@ -32,8 +32,6 @@ public class PerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequ
         if (elapsedMilliseconds > 500)
         {
             var requestName = typeof(TRequest).Name;
-           
-           
 
             _logger.LogWarning("MyWebApi Long Running Request: {Name} ({ElapsedMilliseconds} milliseconds) {@Request}",
                 requestName, elapsedMilliseconds, request);
